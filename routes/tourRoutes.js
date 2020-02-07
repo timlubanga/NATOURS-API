270ac0d0a533a5463e4d62d20697f801c382b422
timothy <timlubanga@gmail.com>
2020-02-07 17:46:19 +0300

    add tour nested routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewRouter = require('../routes/reviewRoutes');
 
 const router = express.Router();
 
@@ -20,6 +21,7 @@ router
   .post(tourController.createTour)
   .delete(tourController.deleteAllTours);
 
+router.use('/:tourId/reviews', reviewRouter);
 router
   .route('/:id')
   .get(tourController.getTour)
